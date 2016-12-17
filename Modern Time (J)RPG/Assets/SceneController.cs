e315469299365f18d72e9943f70380f5bc0f07ea
Zian Wermelinger <zwermi@gmail.com>
2016-12-17 17:59:06 +0100

    Some minor changes to scenes and scriptings

---
+++ Modern Time (J)RPG/Assets/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SceneController : MonoBehaviour {
+
+    
+    public string nameOfScene;
+
+    public void goToScene()
+    { 
+        SceneManager.LoadScene(nameOfScene); 
+    }
+
+    public void Start()
+    {
+       
+    }
+    public void Update()
+    {
+
+    }
+
+}
