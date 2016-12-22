740e356e03c8cdb1de9c030cf6e2a42b95ca03bc
Zian Wermelinger <zwermi@gmail.com>
2016-12-22 15:47:27 +0100

    Re-Import all Economic + NPC changes

--- Modern Time (J)RPG/Assets/Scripts/Characters/NPC/NonPlayerCharacter.cs
+++ Modern Time (J)RPG/Assets/Scripts/Characters/NPC/NonPlayerCharacter.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NonPlayerCharacter : MonoBehaviour {
+public class NonPlayerCharacter : Character {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    int id;
+    Functions func;
+    bool hasFunction;
+
+    public NonPlayerCharacter(int id, string name, bool hasFunction, Functions func)
+    {
+        this.id = id;
+        this.name = name;
+        this.hasFunction = hasFunction;
+        this.func = func;
+
+    }
+
+    public NonPlayerCharacter(int id, string name, bool hasFunction)
+    {
+        this.id = id;
+        this.name = name;
+        this.hasFunction = hasFunction;
+
+    }
+
+    public NonPlayerCharacter()
+    {
+
+    }
+
+    //Getters
+    public int getID() { return id; }
+    public Functions getFunc() { return func; }
+    public bool hasFunctions() { return hasFunction;  }
+
+    //Setters
+    public void setID(int id) { this.id = id; }
+    public void setFunc(Functions func) { this.func = func; }
+    public void setIfFunction(bool hasFunction) { this.hasFunction = hasFunction; }
 }
