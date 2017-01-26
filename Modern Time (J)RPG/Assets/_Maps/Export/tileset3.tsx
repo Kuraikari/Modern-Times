b84f08b465d237f7a92167cd1c800a947f5d6ddd
Zian Wermelinger <zwermi@gmail.com>
2017-01-26 10:57:37 +0100

    re-imported Tiled2Unity + import of the Street Map.

---
+++ Modern Time (J)RPG/Assets/_Maps/Export/tileset3.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset3" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="../../Sprites/tileset3.png" width="256" height="512"/>
+ <terraintypes>
+  <terrain name="Strasse" tile="-1"/>
+ </terraintypes>
+ <tile id="5" terrain="0,0,0,"/>
+ <tile id="6" terrain="0,0,,"/>
+ <tile id="7" terrain="0,0,,0"/>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="6" y="1" width="20.7273" height="26.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="4.90909" y="1" width="20.8182" height="26.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="5.90909" y="1" width="21.0909" height="27.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" terrain="0,,0,"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain=",0,,0"/>
+ <tile id="21" terrain="0,,0,0"/>
+ <tile id="22" terrain=",,0,0"/>
+ <tile id="23" terrain=",0,0,0"/>
+ <tile id="25" terrain="0,0,0,"/>
+ <tile id="26" terrain="0,0,,"/>
+ <tile id="27" terrain="0,0,,0"/>
+ <tile id="29" terrain="0,0,0,"/>
+ <tile id="30" terrain="0,0,,"/>
+ <tile id="31" terrain="0,0,,0"/>
+ <tile id="33" terrain="0,,0,"/>
+ <tile id="34" terrain="0,0,0,0"/>
+ <tile id="35" terrain=",0,,0"/>
+ <tile id="37" terrain="0,,0,"/>
+ <tile id="38" terrain="0,0,0,0"/>
+ <tile id="39" terrain=",0,,0"/>
+ <tile id="41" terrain="0,,0,0"/>
+ <tile id="42" terrain=",,0,0"/>
+ <tile id="43" terrain=",0,0,0"/>
+ <tile id="45" terrain="0,,0,0"/>
+ <tile id="46" terrain=",,0,0"/>
+ <tile id="47" terrain=",0,0,0"/>
+</tileset>
