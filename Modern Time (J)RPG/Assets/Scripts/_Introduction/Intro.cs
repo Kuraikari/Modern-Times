e29e4aebf380b9764107ec62a72e5c929f623c4f
Zian Wermelinger <zwermi@gmail.com>
2017-02-01 15:02:21 +0100

    Intro Scene got some text :3

---
+++ Modern Time (J)RPG/Assets/Scripts/_Introduction/Intro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Intro
+{
+    public List<string> introTexts = new List<string>();
+    public Dictionary<string, string> introQA = new Dictionary<string, string>();
+    public void newTxt(string text) {
+        introTexts.Add(text);
+    }
+
+    public void newQA(string quest, string answer)
+    {
+        introQA.Add(quest, answer);
+    }
+}
---
+++ Modern Time (J)RPG/Assets/Scripts/_Introduction/IntroText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroText : MonoBehaviour {
+
+    public string speaker;
+    public Intro ir;
+    public Text textField;
+
+    public void createIntro()
+    {
+        //Create Intro Text
+        ir.newTxt("Hello, there");
+        ir.newTxt("Welcome to the world of Modern Times!");
+        ir.newTxt("My name is Jirou Youyaka, I'm currently the mayor of a small town and I'm here to introduce you to this world.");
+        ir.newTxt("We were watching you for a long time, Hyuuga. But due to a software problem with our database we lost some data.");
+        ir.newTxt("It would be nice if you could answer our questions to provide us with the missing data. We would appreciate your help.");
+
+        //Create Intro Questions
+        ir.newQA("", "");
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        createIntro();
+    
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject.Find("OutputText").GetComponentInChildren<Text>().text = ir.introTexts[0];
+
+    }
+}
